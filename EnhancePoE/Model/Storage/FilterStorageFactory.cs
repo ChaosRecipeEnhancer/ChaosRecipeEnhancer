6a6e3f18c18b30536ddaf8c251af1ca3a05cf889
zyertdox <7522793+Zyertdox@users.noreply.github.com>
2021-04-12 21:46:56 +0200

    Introduce factory to make it possible to switch source of filter

---
+++ EnhancePoE/Model/Storage/FilterStorageFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace EnhancePoE.Model.Storage
+{
+    public static class FilterStorageFactory
+    {
+        public static IFilterStorage Create(string lootFilterFilePath)
+        {
+            return new FileFilterStorage(lootFilterFilePath);
+        }
+
+        internal static IFilterStorage Create(Properties.Settings settings)
+        {
+            return Create(settings.LootfilterLocation);
+        }
+    }
+}
