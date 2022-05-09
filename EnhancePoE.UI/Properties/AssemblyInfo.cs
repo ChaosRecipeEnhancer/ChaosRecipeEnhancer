1cbaa543e0438615bfaa3e83f865b5ca5a1e505c
Mario Lopez Martinez <mario@lopez.fyi>
2022-05-09 10:53:55 -0500

    Updating copyright year

--- EnhancePoE.UI/Properties/AssemblyInfo.cs
+++ EnhancePoE.UI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EnhancePoE")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
