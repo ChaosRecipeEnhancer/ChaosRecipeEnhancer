e6db4808f65a35ce5c1995ad76c58ba631df655f
Mario Lopez Martinez <mario@lopez.fyi>
2022-08-18 11:36:06 -0500

    Adding some simple settings upgrade logic

--- ChaosRecipeEnhancer.UI/AppBootstrapper.xaml.cs
+++ ChaosRecipeEnhancer.UI/AppBootstrapper.xaml.cs
@@ -29,6 +29,12 @@ namespace ChaosRecipeEnhancer.UI
 
         protected void OnApplicationStartup(object sender, StartupEventArgs e)
         {
+            if (Settings.Default.UpgradeSettingsAfterUpdate)
+            {
+                Settings.Default.Upgrade();
+                Settings.Default.UpgradeSettingsAfterUpdate = false;
+            }
+            
             const string outputTemplate =
                 "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} | Level:{Level} | ThreadId:{ThreadId} | ContextValue:{ContextValue} | SourceContext:{SourceContext}] {Message}{NewLine}{Exception}";
             const string debugOutputTemplate =
