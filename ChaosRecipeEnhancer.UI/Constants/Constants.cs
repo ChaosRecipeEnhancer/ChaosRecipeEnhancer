877c7ecc4fafe1b69889ae4c4cb6ae0a428a0b4f
Mario Lopez Martinez <mario@lopez.fyi>
2023-04-04 21:26:31 -0500

    Adding os specific double line char

--- ChaosRecipeEnhancer.UI/Constants/Constants.cs
+++ ChaosRecipeEnhancer.UI/Constants/Constants.cs
@@ -5,6 +5,7 @@ namespace ChaosRecipeEnhancer.UI.Constants
     public static class StringConstruction
     {
         public static string NewLineCharacter => Environment.NewLine;
+        public static string DoubleNewLineCharacter => Environment.NewLine + Environment.NewLine;
         public const string TabCharacter = "\t";
     }
 
