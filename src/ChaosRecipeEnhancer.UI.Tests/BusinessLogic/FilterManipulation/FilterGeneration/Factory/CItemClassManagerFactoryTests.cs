b8292d4006cfe8ee35e329fa4d8b293cc9399c1b
Ben Younger <hello@benyounger.dev>
2023-08-12 13:09:42 +0100

    Feature: Unit Tests

---
+++ src/ChaosRecipeEnhancer.UI.Tests/BusinessLogic/FilterManipulation/FilterGeneration/Factory/CItemClassManagerFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using ChaosRecipeEnhancer.UI.BusinessLogic.Enums;
+using ChaosRecipeEnhancer.UI.BusinessLogic.FilterManipulation.FilterGeneration.Factory;
+
+namespace ChaosRecipeEnhancer.UI.Tests.BusinessLogic.FilterManipulation.FilterGeneration.Factory;
+
+public class CItemClassManagerFactoryTests
+{
+    [Theory]
+    [InlineData(EnumItemClass.Amulets, "Amulets")]
+    [InlineData(EnumItemClass.Belts, "Belts")]
+    [InlineData(EnumItemClass.BodyArmours, "BodyArmours")]
+    [InlineData(EnumItemClass.Boots, "Boots")]
+    [InlineData(EnumItemClass.Gloves, "Gloves")]
+    [InlineData(EnumItemClass.Helmets, "Helmets")]
+    [InlineData(EnumItemClass.OneHandWeapons, "OneHandWeapons")]
+    [InlineData(EnumItemClass.Rings, "Rings")]
+    [InlineData(EnumItemClass.TwoHandWeapons, "TwoHandWeapons")]
+    public void GetItemClassManager_GivenItemClass_ReturnsManagerWithClassName(EnumItemClass itemClass, string className)
+    {
+        // Arrange
+        var factory = new CItemClassManagerFactory();
+
+        // Act
+        var result = factory.GetItemClassManager(itemClass);
+
+        // Assert
+        result.ClassName.Should().Be(className);
+    }
+
+    [Fact]
+    public void GetItemManager_GivenInvalidClass_Throws()
+    {
+        // Arrange
+        var factory = new CItemClassManagerFactory();
+
+        // Act
+        Action act = () => factory.GetItemClassManager((EnumItemClass) 100000245);
+
+        // Assert
+        act.Should().Throw<Exception>().WithMessage("Wrong item class.");
+    }
+}
---
+++ src/ChaosRecipeEnhancer.UI.Tests/BusinessLogic/FilterManipulation/FilterGeneration/Factory/Managers/ItemClassManager.cs
@@ -0,0 +1,12 @@
+﻿using ChaosRecipeEnhancer.UI.BusinessLogic.FilterManipulation.FilterGeneration.Factory.Managers;
+using ChaosRecipeEnhancer.UI.BusinessLogic.Items;
+
+namespace ChaosRecipeEnhancer.UI.Tests.BusinessLogic.FilterManipulation.FilterGeneration.Factory.Managers;
+
+public class ItemClassManager : ABaseItemClassManager
+{
+    public override ActiveItemTypes SetActiveTypes(ActiveItemTypes activeItems, bool newValue)
+    {
+        return new ActiveItemTypes();
+    }
+}
