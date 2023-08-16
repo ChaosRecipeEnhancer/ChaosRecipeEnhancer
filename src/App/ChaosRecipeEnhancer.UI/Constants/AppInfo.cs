3057b3805534382040442f27b1223464ede63d00
Mario Lopez Martinez <mario@lopez.fyi>
2023-08-16 09:08:47 -0500

    updating project assemblies to reflect v2-beta-1

--- src/App/ChaosRecipeEnhancer.UI/Constants/AppInfo.cs
+++ src/App/ChaosRecipeEnhancer.UI/Constants/AppInfo.cs
@@ -2,7 +2,7 @@
 
 public static class AppInfo
 {
-    public const string VersionText = "v2.0.0-alpha";
+    public const string VersionText = "v2.0.0-beta-1";
     public const string GithubReleasesUrl = "https://github.com/ChaosRecipeEnhancer/EnhancePoEApp/releases";
     public const string DefaultLeagueForHealthCheck = "Standard";
 }
--- src/App/ChaosRecipeEnhancer.UI/Properties/AssemblyInfo.cs
+++ src/App/ChaosRecipeEnhancer.UI/Properties/AssemblyInfo.cs
@@ -1,3 +1,14 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
