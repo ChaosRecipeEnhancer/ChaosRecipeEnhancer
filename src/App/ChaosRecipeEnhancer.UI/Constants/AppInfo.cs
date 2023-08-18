7ee6adb233d57f3dd36822312cef91088fb37a82
Mario Lopez Martinez <mario@lopez.fyi>
2023-08-17 19:20:43 -0500

    updating assembly and app version

--- src/App/ChaosRecipeEnhancer.UI/Constants/AppInfo.cs
+++ src/App/ChaosRecipeEnhancer.UI/Constants/AppInfo.cs
@@ -2,7 +2,7 @@
 
 public static class AppInfo
 {
-    public const string VersionText = "v2.0.0-beta-3";
+    public const string VersionText = "v2.0.0";
     public const string GithubReleasesUrl = "https://github.com/ChaosRecipeEnhancer/EnhancePoEApp/releases";
     public const string DefaultLeagueForHealthCheck = "Standard";
 }
