e57820d02de00a12a224c95b09a4b8218cd3760f
Mario Lopez Martinez <mario@lopez.fyi>
2023-11-29 14:31:58 -0600

    cleaning up more unused files

--- src/API/ChaosRecipeEnhancer.API/Models/ProfileEndpointModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChaosRecipeEnhancer.API.Models;
-
-public class ProfileEndpointModel
-{
-    public string Name { get; set; }
-    public string Realm { get; set; }
-    public string Avatar_url { get; set; }        
-}
