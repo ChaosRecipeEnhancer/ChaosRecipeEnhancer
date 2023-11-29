a158ad4405e286a72a8d1836fb2231d3443953f0
Mario Lopez Martinez <mario@lopez.fyi>
2023-11-29 14:26:37 -0600

    cleaning up solution structure; removing unused files; adding new oauth lambda code; adding test console app to test backend services

--- scripts/infra/synth.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host ""
-Write-Host "** Synthesizing Infrastructure **"
-Write-Host ""
-Write-Host "Environment variables used:"
-Write-Host "===================================="
-Write-Host "LOCAL_PATH: $env:CRE_LOCAL_PATH"
-
-Set-Location $env:CRE_LOCAL_PATH/src/Infra
-npx cdk synth
