f109f90c0a21d0605a40a33ab5420ff6e0f0197b
Mario Lopez Martinez <mario@lopez.fyi>
2023-12-01 20:32:06 -0600

    updating unit tests

--- src/App/ChaosRecipeEnhancer.UI.Tests/Models/EnhancedItemTests.cs
+++ src/App/ChaosRecipeEnhancer.UI.Tests/Models/EnhancedItemTests.cs
@@ -1,6 +1,6 @@
-﻿using ChaosRecipeEnhancer.UI.BusinessLogic.Items;
-using ChaosRecipeEnhancer.UI.Constants;
+﻿using ChaosRecipeEnhancer.UI.Constants;
 using ChaosRecipeEnhancer.UI.Models;
+using ChaosRecipeEnhancer.UI.Models.ApiResponses.BaseModels;
 using ChaosRecipeEnhancer.UI.Tests.Helpers;
 
 namespace ChaosRecipeEnhancer.UI.Tests.Models;
@@ -12,7 +12,17 @@ public class EnhancedItemTests
     public void GetItemClass_GivenURL_AssignsExpectedToItemType(string url, string expected)
     {
         // Arrange
-        var item = new EnhancedItem(1, 1, false, null, 0, 0, 0, new BaseItemInfluences(), url);
+        var item = new EnhancedItem(
+            1,
+            1,
+            false,
+            null,
+            0,
+            0,
+            0,
+            new BaseItemInfluences(),
+            url
+        );
 
         // Act
         item.GetItemClass();
@@ -29,7 +39,17 @@ public class EnhancedItemTests
     public void IsChaosRecipeEligible_GivenItemLevel_ReturnsExpected(int itemLevel, bool expected)
     {
         // Arrange
-        var item = new EnhancedItem(1, 1, false, itemLevel, 0, 0, 0, new BaseItemInfluences(), ItemIconConstants.AmuletUrl);
+        var item = new EnhancedItem(
+            1,
+            1,
+            false,
+            itemLevel,
+            0,
+            0,
+            0,
+            new BaseItemInfluences(),
+            ItemIconConstants.AmuletUrl
+        );
 
         // Act
         var result = item.IsChaosRecipeEligible;
