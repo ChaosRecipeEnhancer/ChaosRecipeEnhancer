e348056542c6761918891140e76f15050ddf360f
Mario Lopez Martinez <mario@lopez.fyi>
2023-12-02 23:01:33 -0600

    overhauling unhandled exception window and adding qol to make it easier to report issues

--- src/App/ChaosRecipeEnhancer.UI/Windows/ErrorWindow.cs
+++ src/App/ChaosRecipeEnhancer.UI/Windows/ErrorWindow.cs
@@ -4,12 +4,18 @@ namespace ChaosRecipeEnhancer.UI.Windows;
 
 public static class ErrorWindow
 {
-    public static void Spawn(string content, string title)
+    public static void Spawn(string content, string title, bool showCopyHint = false)
     {
+        // this is a hint i want to include for long exception messages
+        // makes it easier to do some interactive troubleshooting with users
+        var hint = showCopyHint
+            ? "You can copy the contents of this window by hitting CTRL + C while " +
+              "this window is focused. This will make pasting error messages into " +
+              "Discord, GitHub issues, etc., much easier."
+            : string.Empty;
+
         MessageBox.Show(
-            $"{content}\n\nYou can copy the contents of this window by" +
-            $"hitting CTRL + C while this window is focused. This will make pasting" +
-            $"error messages into Discord, GitHub issues, etc., much easier.",
+            hint + content,
             title,
             MessageBoxButton.OK,
             MessageBoxImage.Error
