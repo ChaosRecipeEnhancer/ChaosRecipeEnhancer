e5ba392961372c70cb78001262a4a8a1a9785c3c
Mario Lopez Martinez <mario@lopez.fyi>
2023-12-02 18:56:09 -0600

    adding msg to clarify you can copy (error) window msg easily

--- src/App/ChaosRecipeEnhancer.UI/Windows/ErrorWindow.cs
+++ src/App/ChaosRecipeEnhancer.UI/Windows/ErrorWindow.cs
@@ -4,8 +4,15 @@ namespace ChaosRecipeEnhancer.UI.Windows;
 
 public static class ErrorWindow
 {
-	public static void Spawn(string content, string title)
-	{
-		MessageBox.Show(content, title, MessageBoxButton.OK, MessageBoxImage.Error);
-	}
+    public static void Spawn(string content, string title)
+    {
+        MessageBox.Show(
+            $"{content}\n\nYou can copy the contents of this window by" +
+            $"hitting CTRL + C while this window is focused. This will make pasting" +
+            $"error messages into Discord, GitHub issues, etc., much easier.",
+            title,
+            MessageBoxButton.OK,
+            MessageBoxImage.Error
+        );
+    }
 }
