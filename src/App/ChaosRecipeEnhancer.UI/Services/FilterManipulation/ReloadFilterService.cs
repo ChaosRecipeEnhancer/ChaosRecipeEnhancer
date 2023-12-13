bbcbaab34903929b91b8a472f9c155034b64fb5e
Tiago dos Santos Rodrigues <sharps@live.com>
2023-12-13 15:12:35 +0000

    Using SendKeys.SendWait on a separated thread

--- src/App/ChaosRecipeEnhancer.UI/Services/FilterManipulation/ReloadFilterService.cs
+++ src/App/ChaosRecipeEnhancer.UI/Services/FilterManipulation/ReloadFilterService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using ChaosRecipeEnhancer.UI.Properties;
 using ChaosRecipeEnhancer.UI.Utilities.Native;
@@ -8,66 +9,69 @@ using ChaosRecipeEnhancer.UI.Windows;
 
 namespace ChaosRecipeEnhancer.UI.Services.FilterManipulation;
 
-public interface IReloadFilterService
+public interface IReloadFilterService 
 {
-    public void ReloadFilter();
+	public void ReloadFilter();
 }
 
-public class ReloadFilterService : IReloadFilterService
+public class ReloadFilterService : IReloadFilterService 
 {
-    public void ReloadFilter()
-    {
-        var chatCommand = BuildFilterReloadCommand();
-        if (chatCommand is null) return;
+	public void ReloadFilter() 
+	{
+		var chatCommand = BuildFilterReloadCommand();
+		if (chatCommand is null) return;
 
-        // Map all current window names to their associated "handle to a window" pointers (HWND)
-        var openWindows = WindowsUtilitiesForOverlays.GetOpenWindows();
+		// Map all current window names to their associated "handle to a window" pointers (HWND)
+		var openWindows = WindowsUtilitiesForOverlays.GetOpenWindows();
 
-        foreach (var window in openWindows)
-        {
-            var handle = window.Key;
-            var title = window.Value;
+		foreach (var window in openWindows) 
+		{
+			var handle = window.Key;
+			var title = window.Value;
 
-            Console.WriteLine("{0}: {1}", handle, title);
-        }
+			Console.WriteLine("{0}: {1}", handle, title);
+		}
 
-        // Find the Process ID associated with the 'Path of Exile' game window
-        var poeWindow = openWindows.FirstOrDefault(x => x.Value == "Path of Exile").Key;
+		// Find the Process ID associated with the 'Path of Exile' game window
+		var poeWindow = openWindows.FirstOrDefault(x => x.Value == "Path of Exile").Key;
 
-        if (WindowsUtilitiesForOverlays.CheckIfWindowExists(poeWindow))
-        {
-            ErrorWindow.Spawn(
-                "Could not find PoE window! Please make sure PoE is running.\n\n" +
-                " If PoE is running in admin mode, try running our app in admin mode, as well.",
-                "Error: Reload Filter - PoE Window Not Found"
-            );
+		if (WindowsUtilitiesForOverlays.CheckIfWindowExists(poeWindow))
+		{
+			ErrorWindow.Spawn(
+				"Could not find PoE window! Please make sure PoE is running.\n\n" +
+				" If PoE is running in admin mode, try running our app in admin mode, as well.",
+				"Error: Reload Filter - PoE Window Not Found"
+			);
 
-            return;
-        }
+			return;
+		}
 
-        // Get 'Path of Exile' window in the foreground to actually send input to said window
-        WindowsUtilitiesForOverlays.SetForegroundWindow(poeWindow);
+		// Get 'Path of Exile' window in the foreground to actually send input to said window
+		WindowsUtilitiesForOverlays.SetForegroundWindow(poeWindow);
 
-        // Always clear the clipboard first
-        Clipboard.Clear();
-        Clipboard.SetText(chatCommand);
+		// Always clear the clipboard first
+		Clipboard.Clear();
+		Clipboard.SetText(chatCommand);
 
-        SendKeys.SendWait("{ENTER}");
-        SendKeys.SendWait("^(v)");
-        SendKeys.SendWait("{ENTER}");
-    }
+		Task.Run(() => 
+		{
+			SendKeys.SendWait("{ENTER}");
+			SendKeys.SendWait("^(v)");
+			SendKeys.SendWait("{ENTER}");
+		});
+	}
 
-    private string BuildFilterReloadCommand()
-    {
-        var filterName = GetFilterName();
+	private string BuildFilterReloadCommand() 
+	{
+		var filterName = GetFilterName();
 
-        if (!string.IsNullOrEmpty(filterName)) return "/itemfilter " + filterName;
+		if (!string.IsNullOrEmpty(filterName)) return "/itemfilter " + filterName;
 
-        return null;
-    }
+		return null;
+	}
 
-    private string GetFilterName()
-    {
-        return Path.GetFileName(Settings.Default.LootFilterFileLocation)!.Replace(".filter", "");
-    }
+	private string GetFilterName()
+	{
+		return Path.GetFileName(Settings.Default.LootFilterFileLocation)!.Replace(".filter", "");
+	}
 }
