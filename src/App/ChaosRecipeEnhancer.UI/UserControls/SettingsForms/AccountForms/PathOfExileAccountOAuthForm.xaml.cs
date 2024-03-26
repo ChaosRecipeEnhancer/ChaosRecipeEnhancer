e8e264b7d746c1d0ab8bf1c3bb0a2fbae2753cf5
Mario Lopez Martinez <mario@lopez.fyi>
2024-03-26 14:47:46 -0500

    big refactors to oauth form (more mvvm-ish)

--- src/App/ChaosRecipeEnhancer.UI/UserControls/SettingsForms/AccountForms/PathOfExileAccountOAuthForm.xaml.cs
+++ src/App/ChaosRecipeEnhancer.UI/UserControls/SettingsForms/AccountForms/PathOfExileAccountOAuthForm.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System.Windows;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
 using System.Windows.Controls;
 
 namespace ChaosRecipeEnhancer.UI.UserControls.SettingsForms.AccountForms;
 
-/// <summary>
-/// Interaction logic for PathOfExileAccountOAuthForm.xaml
-/// </summary>
 public partial class PathOfExileAccountOAuthForm : UserControl
 {
-    private readonly PathOfExileAccountOAuthFormViewModel _model;
-
     public PathOfExileAccountOAuthForm()
     {
         InitializeComponent();
-        DataContext = _model = new PathOfExileAccountOAuthFormViewModel();
-    }
-
-    private void OnLoginClicked(object sender, RoutedEventArgs e)
-    {
-        PathOfExileAccountOAuthFormViewModel.LoginToPoEWebsite();
-    }
-
-    private void OnLogoutClicked(object sender, RoutedEventArgs e)
-    {
-        PathOfExileAccountOAuthFormViewModel.Logout();
+        DataContext = Ioc.Default.GetRequiredService<PathOfExileAccountOAuthFormViewModel>();
     }
 }
