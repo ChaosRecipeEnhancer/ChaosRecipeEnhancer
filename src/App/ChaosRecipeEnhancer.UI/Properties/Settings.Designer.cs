160f861640e9bfbbe8b610dd1b0412c0cf7897bf
Mario Lopez Martinez <mario@lopez.fyi>
2024-03-26 15:40:48 -0500

    removing custom league flag from settings

--- src/App/ChaosRecipeEnhancer.UI/Properties/Settings.Designer.cs
+++ src/App/ChaosRecipeEnhancer.UI/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ChaosRecipeEnhancer.UI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -92,18 +92,6 @@ namespace ChaosRecipeEnhancer.UI.Properties {
             }
             set {
                 this["LeagueName"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool CustomLeagueEnabled {
-            get {
-                return ((bool)(this["CustomLeagueEnabled"]));
-            }
-            set {
-                this["CustomLeagueEnabled"] = value;
             }
         }
         
@@ -799,6 +787,18 @@ namespace ChaosRecipeEnhancer.UI.Properties {
             }
             set {
                 this["SilenceSetsFullMessage"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public int PathOfExileClientLogLocationMode {
+            get {
+                return ((int)(this["PathOfExileClientLogLocationMode"]));
+            }
+            set {
+                this["PathOfExileClientLogLocationMode"] = value;
             }
         }
     }
