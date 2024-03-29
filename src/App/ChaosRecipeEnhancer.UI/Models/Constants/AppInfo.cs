9500bb308b67b5a46117d117a74e1f86202e9271
Mario Lopez Martinez <mario@lopez.fyi>
2024-03-29 01:06:10 -0500

    adding some constants

--- src/App/ChaosRecipeEnhancer.UI/Models/Constants/AppInfo.cs
+++ src/App/ChaosRecipeEnhancer.UI/Models/Constants/AppInfo.cs
@@ -2,8 +2,10 @@
 
 public static class AppInfo
 {
+    public const string AppInstanceName = "ChaosRecipeEnhancer";
     public const string VersionText = "v3.24.1001";
     public const string VersionTextNoPrefix = "3.24.1001";
     public const string GitHubOrgName = "ChaosRecipeEnhancer";
     public const string GithubRepoName = "ChaosRecipeEnhancer";
+    public const string CreProtocolPrefix = "chaosrecipe://";
 }
