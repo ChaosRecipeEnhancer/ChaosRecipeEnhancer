93d87d5df20fe0be70a4c88b3c374d65bb1de97c
Mario Lopez Martinez <mario@lopez.fyi>
2024-03-28 23:13:49 -0500

    updating app assemblies

--- src/App/ChaosRecipeEnhancer.UI/Models/Constants/AppInfo.cs
+++ src/App/ChaosRecipeEnhancer.UI/Models/Constants/AppInfo.cs
@@ -2,8 +2,8 @@
 
 public static class AppInfo
 {
-    public const string VersionText = "v3.24.1000";
-    public const string VersionTextNoPrefix = "3.24.1000";
+    public const string VersionText = "v3.24.1001";
+    public const string VersionTextNoPrefix = "3.24.1001";
     public const string GitHubOrgName = "ChaosRecipeEnhancer";
-    public const string GithubRepoName = "EnhancePoEApp";
+    public const string GithubRepoName = "ChaosRecipeEnhancer";
 }
