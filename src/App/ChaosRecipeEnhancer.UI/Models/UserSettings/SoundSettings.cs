73f346ed7c70c48e87257065e9b07abad41a6600
Mario Lopez Martinez <mario@lopez.fyi>
2024-03-29 01:02:12 -0500

    implementing most settings in UserSettings

---
+++ src/App/ChaosRecipeEnhancer.UI/Models/UserSettings/SoundSettings.cs
@@ -0,0 +1,35 @@
+﻿using ChaosRecipeEnhancer.UI.Properties;
+
+namespace ChaosRecipeEnhancer.UI.Models.UserSettings;
+
+/// <summary>
+/// User Settings related to the Sound Settings.
+/// </summary>
+public partial class UserSettings : IUserSettings
+{
+    public bool SoundEnabled
+    {
+        get => Settings.Default.SoundEnabled;
+        set
+        {
+            if (Settings.Default.SoundEnabled != value)
+            {
+                Settings.Default.SoundEnabled = value;
+                Save();
+            }
+        }
+    }
+
+    public double SoundLevel
+    {
+        get => Settings.Default.SoundLevel;
+        set
+        {
+            if (Settings.Default.SoundLevel != value)
+            {
+                Settings.Default.SoundLevel = value;
+                Save();
+            }
+        }
+    }
+}
