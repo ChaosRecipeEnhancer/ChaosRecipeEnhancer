b4e158ad2b47dea7a5df133961375ee60017dc90
Mario Lopez Martinez <mario@lopez.fyi>
2024-04-03 15:15:33 -0500

    correcting logging config (that doesn't require admin access) cont.

--- src/App/ChaosRecipeEnhancer.UI/Configuration/LoggingConfiguration.cs
+++ src/App/ChaosRecipeEnhancer.UI/Configuration/LoggingConfiguration.cs
@@ -10,18 +10,6 @@ public static class LoggingConfiguration
 {
     public static void ConfigureSerilogLogging()
     {
-        // Getting the path to the Local AppData folder
-        var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        var logDirectory = Path.Combine(appDataPath, "ChaosRecipeEnhancer", "Logs");
-
-        // Ensure the directory exists
-        if (!Directory.Exists(logDirectory))
-        {
-            Directory.CreateDirectory(logDirectory);
-        }
-
-        var logFilePath = Path.Combine(logDirectory, "log.txt");
-
         var logConfiguration = new LoggerConfiguration()
             .MinimumLevel.Information()
             .Enrich.WithProperty("CreAppVersion", CreAppConfig.VersionText)
@@ -29,6 +17,18 @@ public static class LoggingConfiguration
 
         if (Settings.Default.DebugMode)
         {
+            // Getting the path to the Local AppData folder
+            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var logDirectory = Path.Combine(appDataPath, "ChaosRecipeEnhancer", "Logs");
+
+            // Ensure the directory exists
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            var logFilePath = Path.Combine(logDirectory, "log.txt");
+
             logConfiguration.WriteTo.File(logFilePath, rollingInterval: RollingInterval.Hour,
                 outputTemplate: "[CRE {CreAppVersion}] {Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}");
         }
