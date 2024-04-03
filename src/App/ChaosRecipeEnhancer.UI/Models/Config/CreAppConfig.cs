f4b037a360f25fa8f2576bdc20a87125f473aa9f
Mario Lopez Martinez <mario@lopez.fyi>
2024-04-03 15:12:32 -0500

    updating app assembly versions and version references

--- src/App/ChaosRecipeEnhancer.UI/Models/Config/CreAppConfig.cs
+++ src/App/ChaosRecipeEnhancer.UI/Models/Config/CreAppConfig.cs
@@ -4,7 +4,7 @@ public static class CreAppConfig
 {
     public const string InstanceName = "ChaosRecipeEnhancer";
     public const string ProtocolPrefix = "chaosrecipe://";
-    public const string VersionText = "3.24.3001";
+    public const string VersionText = "3.24.3002";
     public const string GitHubOrgName = "ChaosRecipeEnhancer";
     public const string GithubRepoName = "ChaosRecipeEnhancer";
 }
