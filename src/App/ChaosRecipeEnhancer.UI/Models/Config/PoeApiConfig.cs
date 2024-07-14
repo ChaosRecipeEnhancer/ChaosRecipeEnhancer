ada21fec786e2bed12ca13b18946ff0252a176ac
Mario Lopez Martinez <mario@lopez.fyi>
2024-07-14 10:16:40 -0500

    feat(app): adding session id endpoint routes

--- src/App/ChaosRecipeEnhancer.UI/Models/Config/PoeApiConfig.cs
+++ src/App/ChaosRecipeEnhancer.UI/Models/Config/PoeApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using ChaosRecipeEnhancer.UI.Properties;
+﻿using ChaosRecipeEnhancer.UI.Models.Enums;
+using ChaosRecipeEnhancer.UI.Properties;
 using System;
 using System.Reflection;
 
@@ -7,20 +8,53 @@ namespace ChaosRecipeEnhancer.UI.Models.Config;
 public static class PoeApiConfig
 {
     public const string PoeApiHttpClientName = "PoEApiClient";
+    public static string UserAgent
+        => $"OAuth chaosrecipeenhancer/{Assembly.GetExecutingAssembly().GetName().Version} (contact: dev@chaos-recipe.com)";
 
-    private const string PoeApiBaseUrl = "https://api.pathofexile.com";
+    #region Public (no auth required) Endpoints
 
     public static readonly Uri PublicLeagueEndpoint = new("https://api.pathofexile.com/leagues?type=main&realm=pc");
 
-    public static Uri PersonalLeaguesEndpoint()
-        => new($"{PoeApiBaseUrl}/account/leagues");
+    #endregion
 
-    public static Uri PersonalStashTabPropsEndpoint()
-        => new($"{PoeApiBaseUrl}/stash/{Settings.Default.LeagueName}");
+    #region Session ID Endpoints
 
-    public static Uri PersonalIndividualTabContentsEndpoint(string stashTabId)
-        => new($"{PoeApiBaseUrl}/stash/{Settings.Default.LeagueName}/{stashTabId}");
+    public static Uri IndividualTabContentsSessionIdEndpoint(TargetStash targetStash, int stashTabIndex) =>
+        targetStash == TargetStash.Personal
+            ? PersonalStashIndividualTabSessionIdEndpoint(stashTabIndex)
+            : GuildStashIndividualTabSessionIdEndpoint(stashTabIndex);
 
-    public static string UserAgent
-        => $"OAuth chaosrecipeenhancer/{Assembly.GetExecutingAssembly().GetName().Version} (contact: dev@chaos-recipe.com)";
+    private static Uri PersonalStashIndividualTabSessionIdEndpoint(int stashTabIndex) =>
+        new($"https://www.pathofexile.com/character-window/get-stash-items?accountName={Settings.Default.LegacyAuthAccountName}&realm=pc&league={Settings.Default.LeagueName}&tabIndex={stashTabIndex}");
+
+    private static Uri GuildStashIndividualTabSessionIdEndpoint(int stashTabIndex) =>
+        new($"https://www.pathofexile.com/character-window/get-guild-stash-items?accountName={Settings.Default.LegacyAuthAccountName}&realm=pc&league={Settings.Default.LeagueName}&tabIndex={stashTabIndex}");
+
+    public static Uri StashTabPropsSessionIdEndpoint(TargetStash targetStash) =>
+        targetStash == TargetStash.Personal
+            ? PersonalStashTabPropsSessionIdEndpoint()
+            : GuildStashTabPropsSessionIdEndpoint();
+
+    private static Uri PersonalStashTabPropsSessionIdEndpoint() =>
+        new($"https://www.pathofexile.com/character-window/get-stash-items?accountName={Settings.Default.LegacyAuthAccountName}&league={Settings.Default.LeagueName}&tabs=1&tabIndex=");
+
+    private static Uri GuildStashTabPropsSessionIdEndpoint() =>
+        new($"https://www.pathofexile.com/character-window/get-guild-stash-items?accountName={Settings.Default.LegacyAuthAccountName}&league={Settings.Default.LeagueName}&tabs=1&tabIndex=");
+
+    #endregion
+
+    #region OAuth 2.0 Endpoints
+
+    private const string PoeOAuthApiBaseUrl = "https://api.pathofexile.com";
+
+    public static Uri PersonalLeaguesOAuthEndpoint()
+        => new($"{PoeOAuthApiBaseUrl}/account/leagues");
+
+    public static Uri PersonalStashTabPropsOAuthEndpoint()
+        => new($"{PoeOAuthApiBaseUrl}/stash/{Settings.Default.LeagueName}");
+
+    public static Uri PersonalIndividualTabContentsOAuthEndpoint(string stashTabId)
+        => new($"{PoeOAuthApiBaseUrl}/stash/{Settings.Default.LeagueName}/{stashTabId}");
+
+    #endregion
 }
