f3b4da7a58d9caf1ad63d151a7a6621f5f706fac
Mario Lopez Martinez <mario@lopez.fyi>
2024-07-17 11:04:07 -0500

    feat(app): renaming some settings model files

---
+++ src/App/ChaosRecipeEnhancer.UI/Models/UserSettings/AutoFetchSettings.cs
@@ -0,0 +1,48 @@
+﻿using ChaosRecipeEnhancer.UI.Properties;
+
+namespace ChaosRecipeEnhancer.UI.Models.UserSettings;
+
+/// <summary>
+/// User Settings related to the Chaos Recipe Enhancer Auto-Fetch Settings.
+/// </summary>
+public partial class UserSettings : IUserSettings
+{
+    public bool AutoFetchOnRezoneEnabled
+    {
+        get => Settings.Default.AutoFetchOnRezoneEnabled;
+        set
+        {
+            if (Settings.Default.AutoFetchOnRezoneEnabled != value)
+            {
+                Settings.Default.AutoFetchOnRezoneEnabled = value;
+                Save();
+            }
+        }
+    }
+
+    public int PathOfExileClientLogLocationMode
+    {
+        get => Settings.Default.PathOfExileClientLogLocationMode;
+        set
+        {
+            if (Settings.Default.PathOfExileClientLogLocationMode != value)
+            {
+                Settings.Default.PathOfExileClientLogLocationMode = value;
+                Save();
+            }
+        }
+    }
+
+    public string PathOfExileClientLogLocation
+    {
+        get => Settings.Default.PathOfExileClientLogLocation;
+        set
+        {
+            if (Settings.Default.PathOfExileClientLogLocation != value)
+            {
+                Settings.Default.PathOfExileClientLogLocation = value;
+                Save();
+            }
+        }
+    }
+}
--- src/App/ChaosRecipeEnhancer.UI/Models/UserSettings/StashSettings.cs
+++ src/App/ChaosRecipeEnhancer.UI/Models/UserSettings/StashSettings.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace ChaosRecipeEnhancer.UI.Models.UserSettings;
 
 /// <summary>
-/// User Settings related to the Chaos Recipe Enhancer General Settings.
+/// User Settings related to the Chaos Recipe Enhancer Stash Settings.
 /// </summary>
 public partial class UserSettings : IUserSettings
 {
@@ -85,45 +85,6 @@ public partial class UserSettings : IUserSettings
             if (Settings.Default.StashTabQueryMode != value)
             {
                 Settings.Default.StashTabQueryMode = value;
-                Save();
-            }
-        }
-    }
-
-    public bool AutoFetchOnRezoneEnabled
-    {
-        get => Settings.Default.AutoFetchOnRezoneEnabled;
-        set
-        {
-            if (Settings.Default.AutoFetchOnRezoneEnabled != value)
-            {
-                Settings.Default.AutoFetchOnRezoneEnabled = value;
-                Save();
-            }
-        }
-    }
-
-    public int PathOfExileClientLogLocationMode
-    {
-        get => Settings.Default.PathOfExileClientLogLocationMode;
-        set
-        {
-            if (Settings.Default.PathOfExileClientLogLocationMode != value)
-            {
-                Settings.Default.PathOfExileClientLogLocationMode = value;
-                Save();
-            }
-        }
-    }
-
-    public string PathOfExileClientLogLocation
-    {
-        get => Settings.Default.PathOfExileClientLogLocation;
-        set
-        {
-            if (Settings.Default.PathOfExileClientLogLocation != value)
-            {
-                Settings.Default.PathOfExileClientLogLocation = value;
                 Save();
             }
         }
