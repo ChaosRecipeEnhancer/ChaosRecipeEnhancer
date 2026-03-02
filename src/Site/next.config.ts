51f1e845ba5fc7f441eed82d9b9b251e58ad6d22
Mario Lopez Martinez <mario@lopez.fyi>
2026-03-02 17:11:47 -0600

    fix: next rewrites

--- src/Site/next.config.ts
+++ src/Site/next.config.ts
@@ -1,5 +1,25 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/auth/success",
+        destination:
+          "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/auth/success",
+      },
+      {
+        source: "/auth/token",
+        destination:
+          "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/auth/token",
+      },
+      {
+        source: "/v2/auth/token",
+        destination:
+          "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/v2/auth/token",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
