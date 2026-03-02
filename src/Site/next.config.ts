3f8e9d66799316788b1559c09f51f023dcad1673
Mario Lopez Martinez <mario@lopez.fyi>
2026-03-02 17:15:24 -0600

    fix: go agane

--- src/Site/next.config.ts
+++ src/Site/next.config.ts
@@ -2,23 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/auth/success",
-        destination:
-          "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/auth/success",
-      },
-      {
-        source: "/auth/token",
-        destination:
-          "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/auth/token",
-      },
-      {
-        source: "/v2/auth/token",
-        destination:
-          "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/v2/auth/token",
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: "/auth/success",
+          destination:
+            "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/auth/success",
+        },
+        {
+          source: "/auth/token",
+          destination:
+            "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/auth/token",
+        },
+        {
+          source: "/v2/auth/token",
+          destination:
+            "https://d-rqtq07y5g9.execute-api.us-east-1.amazonaws.com/v2/auth/token",
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
   },
 };
 
